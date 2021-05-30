32ca2493c618ce10bb43d84f9830d4388ffdb02b
Michael Nel <neckware@gmail.com>
2021-05-30 14:29:28 -0300

    feature test integration cep service

---
+++ WebClientOrder.Test/Integration/Service/CepServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WebClientOrder.Domain.Services;
+using WebClientOrder.Infra.Services;
+using Xunit;
+
+namespace WebClientOrder.Test.Integration.Service
+{
+    public class CepServiceTest
+    {
+        private readonly ICepService _cepService;
+        public CepServiceTest()
+        {
+            var appSettings = new Dictionary<string, string> {
+                {"Cep:Api", "https://viacep.com.br/ws"},
+            };
+
+            var appConfiguration = new ConfigurationBuilder().AddInMemoryCollection(appSettings).Build();
+
+            _cepService = new CepService(appConfiguration);
+        }
+
+
+        [Fact]
+
+        public async Task ShouldBe_Get_Address_With_Cep()
+        {
+            var address = await _cepService.Execute("23575460");
+
+            Assert.Equal("23575-460", address.ZipCode);
+            Assert.Equal("Rua Cristo Redentor", address.Street);
+            Assert.Equal("Paciência", address.District);
+            Assert.Equal("Rio de Janeiro", address.City);
+            Assert.Equal("RJ", address.State);
+        }
+
+    }
+}
